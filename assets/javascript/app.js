7213e0010db85729e823116f472c4b6a07784c0b
Kyle C <kylecilia@gmail.com>
2019-10-22 19:38:40 -0700

    [WIP] Starting to set up gameState object, added jQuery selectors in display sub object

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var gameState = {
+  display: {
+    timer: $(".timer-text"),
+    question: $(".question-text"),
+    answer1: $(".answer-text-1"),
+    answer2: $(".answer-text-2"),
+    answer3: $(".answer-text-3"),
+    answer4: $(".answer-text-4"),
+    
+  },
+};
