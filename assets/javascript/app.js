5118b5e3eb7fa3db297612a11a46da2cc37da68a
Kyle C <kylecilia@gmail.com>
2019-10-23 18:14:01 -0700

    Update and click function added, countDown() not working because twentyFiveSec not being interpreted as a num

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,4 +8,43 @@ var gameState = {
     answer4: $(".answer-text-4"),
     
   },
+
+  timer: {
+    twentyFiveSec: 25,
+    countDown() {
+      gameState.display.timer.text(this.twentyFiveSec);
+      this.twentyFiveSec--;
+      console.log('timer.countDown', this.twentyFiveSec);
+      if(this.twentyFiveSec == 0) {
+        console.log("Time's up!");
+      }
+    },
+  },
+
+  update() {
+    $(document).ready(function() {
+      gameState.click();
+      setInterval(gameState.timer.countDown, 1000);
+    }); 
+  },
+
+  click() {
+    this.display.answer1.click(function() {
+      console.log('Clicked answer 1');
+    });
+
+    this.display.answer2.click(function() {
+      console.log('Clicked answer 2');
+    });
+
+    this.display.answer3.click(function(){
+      console.log('Clicked answer 3');
+    });
+
+    this.display.answer4.click(function() {
+      console.log('Clicked answer 4');
+    });
+  }
 };
+
+gameState.update();
